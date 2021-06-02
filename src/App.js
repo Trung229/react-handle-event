c4cb7d704058ee79fe5e67df4af3a970e323f9de
Trung <ductrung4263@gmail.com>
2021-06-02 17:35:10 +0700

    handleEventListener

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [change, setChange] = useState("");
+  const handleChange =(e)=>{
+    setChange(e.target.value);
+  }  
+  const handleClick =(e)=>{
+    alert("you just type" + change)
+  }
+  const handleMouseOver =(e)=>{
+    alert(e.target.childNodes[0].data)
+  }
+  return(
+    <div>
+      <input placeholder="Enter whatever you want" onChange={handleChange} value={change}></input>
+      <p>{change}</p>
+      <button onClick={handleClick}>click me</button>
+      <p onMouseOver={handleMouseOver}> choose me</p>
     </div>
-  );
+  )
 }
-
 export default App;
